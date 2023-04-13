087e03c4349c37a987ae7ad9e42d4572ff69a01e
Ole Ildsgaard Hougaard <oih@via.dk>
2023-04-13 11:30:52 +0200

    Making the returned randomWord() predictable

--- offline-wordle/src/dk/via/wordle/FakeRandomDictionary.java
+++ offline-wordle/src/dk/via/wordle/FakeRandomDictionary.java
@@ -3,13 +3,14 @@ package dk.via.wordle;
 import java.rmi.RemoteException;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Random;
 
 public class FakeRandomDictionary implements Dictionary {
     private final HashSet<String> words;
 
     public FakeRandomDictionary(String... words) {
-        this.words = new HashSet<>(Arrays.asList(words));
+        this.words = new LinkedHashSet<>(Arrays.asList(words));
     }
 
     @Override
